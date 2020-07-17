a53faf5d967a6301ddcaa90935eca00bb833ceff
Wouter van der Wal <33957974+wjtje@users.noreply.github.com>
2020-07-17 10:06:52 +0200

    Changed the most used response

--- src/location/user.ts
+++ src/location/user.ts
@@ -28,9 +28,17 @@ new Api({
   url: '/location/user/:user_id/most',
   auth: true,
   get: (request, response, userInfo) => {
+    const blank = {
+      location_id: 0,
+      name: String(undefined),
+      place: String(undefined),
+      id: 0,
+      occurrence: 0,
+    }
+
     // Get most used location_id from the server (limit 2)
     DBcon.query(
-      "SELECT `location_id`, COUNT(`location_id`) as `occurrence` FROM `TL_work` WHERE `user_id` = ? AND `date` >= ? AND `date` <= ? GROUP BY `location_id` ORDER BY `occurrence` DESC LIMIT 2",
+      "SELECT `location_id`, `name`, `place`, `id`, COUNT(`location_id`) as `occurrence` FROM `TL_work` INNER JOIN `TL_locations` USING (`location_id`) WHERE `user_id` = ? AND `date` >= ? AND `date` <= ? GROUP BY `location_id` ORDER BY `occurrence` DESC LIMIT 2",
       [
         userInfo.user_id,
         moment().subtract(7, 'days').format("YYYY-MM-DD"),  // Last week
@@ -39,8 +47,8 @@ new Api({
       handleQuery(response, (result) => {
         responseDone(response, {
           location_id: [
-            _.get(result, '[0].location_id', 0),
-            _.get(result, '[1].location_id', 0),
+            _.get(result, '[0]', blank),
+            _.get(result, '[1]', blank),
           ]
         })
       })

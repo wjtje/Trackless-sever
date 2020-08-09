14404173b347da89de70339e15392fdd159740a9
Wouter van der Wal <33957974+wjtje@users.noreply.github.com>
2020-08-09 12:29:37 +0200

    Update /location/user/:user_id/most

--- src/location/user.ts
+++ src/location/user.ts
@@ -2,8 +2,11 @@ import Api from "../scripts/api";
 import { DBcon } from "..";
 import { handleQuery } from "../scripts/handle";
 import { responseDone } from "../scripts/response";
-import { get as _get } from "lodash";
+import { get as _get, result } from "lodash";
 import moment from "moment";
+import { promisify } from 'util';
+
+const query = promisify(DBcon.query).bind(DBcon);
 
 // Get last used location for that user
 new Api({
@@ -28,30 +31,34 @@ new Api({
   url: '/location/user/:user_id/most',
   auth: true,
   get: (request, response, userInfo) => {
-    const blank = {
-      location_id: 0,
-      name: String(undefined),
-      place: String(undefined),
-      id: 0,
-      occurrence: 0,
-    }
-
-    // Get most used location_id from the server (limit 2)
+    // Get two random locations
     DBcon.query(
-      "SELECT `location_id`, `name`, `place`, `id`, COUNT(`location_id`) as `occurrence` FROM `TL_work` INNER JOIN `TL_locations` USING (`location_id`) WHERE `user_id` = ? AND `date` >= ? AND `date` <= ? GROUP BY `location_id` ORDER BY `occurrence` DESC LIMIT 2",
-      [
-        userInfo.user_id,
-        moment().subtract(7, 'days').format("YYYY-MM-DD"),  // Last week
-        moment().format("YYYY-MM-DD"),                      // Now
-      ],
-      handleQuery(response, (result) => {
-        responseDone(response, {
-          location_id: [
-            _get(result, '[0]', blank),
-            _get(result, '[1]', blank),
-          ]
-        })
+      "SELECT r1.location_id, r1.name, r1.place, r1.id FROM `TL_locations` AS r1 JOIN (SELECT CEIL(RAND() * (SELECT MAX(location_id) FROM `TL_locations`)) AS location_id) AS r2 WHERE r1.location_id >= r2.location_id ORDER BY r1.location_id ASC LIMIT 2",
+      handleQuery(response, (randomLocations) => {
+        // Get most used location_id from the server (limit 2)
+        DBcon.query(
+          "SELECT `location_id`, `name`, `place`, `id`, COUNT(`location_id`) as `occurrence` FROM `TL_work` INNER JOIN `TL_locations` USING (`location_id`) WHERE `user_id` = ? AND `date` >= ? AND `date` <= ? GROUP BY `location_id` ORDER BY `occurrence` DESC LIMIT 2",
+          [
+            userInfo.user_id,
+            moment().subtract(7, 'days').format("YYYY-MM-DD"),  // Last week
+            moment().format("YYYY-MM-DD"),                      // Now
+          ],
+          handleQuery(response, (result) => {
+            responseDone(response, {
+              location_id: [
+                _get(result, '[0]', {
+                  ...randomLocations[0],
+                  occurrence: 0,
+                }),
+                _get(result, '[1]', {
+                  ...randomLocations[1],
+                  occurrence: 0,
+                }),
+              ]
+            })
+          })
+        )
       })
-    );
+    )
   }
 })
